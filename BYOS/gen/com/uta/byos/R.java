e93e44a4b53904d867bedd032b701f9e24eed180
DallasWildman <matthew.waller@mavs.uta.edu>
2013-02-17 15:51:48 -0600

    First commit with proper configs

--- BYOS/gen/com/uta/byos/R.java
+++ BYOS/gen/com/uta/byos/R.java
@@ -19,23 +19,79 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int dummy_button=0x7f080002;
+        public static final int fullscreen_content=0x7f080000;
+        public static final int fullscreen_content_controls=0x7f080001;
     }
     public static final class layout {
         public static final int activity_tableau=0x7f030000;
     }
+    public static final class raw {
+        public static final int cardback=0x7f040000;
+        public static final int club1=0x7f040001;
+        public static final int club10=0x7f040002;
+        public static final int club11=0x7f040003;
+        public static final int club12=0x7f040004;
+        public static final int club13=0x7f040005;
+        public static final int club2=0x7f040006;
+        public static final int club3=0x7f040007;
+        public static final int club4=0x7f040008;
+        public static final int club5=0x7f040009;
+        public static final int club6=0x7f04000a;
+        public static final int club7=0x7f04000b;
+        public static final int club8=0x7f04000c;
+        public static final int club9=0x7f04000d;
+        public static final int diamond1=0x7f04000e;
+        public static final int diamond10=0x7f04000f;
+        public static final int diamond11=0x7f040010;
+        public static final int diamond12=0x7f040011;
+        public static final int diamond13=0x7f040012;
+        public static final int diamond2=0x7f040013;
+        public static final int diamond3=0x7f040014;
+        public static final int diamond4=0x7f040015;
+        public static final int diamond5=0x7f040016;
+        public static final int diamond6=0x7f040017;
+        public static final int diamond7=0x7f040018;
+        public static final int diamond8=0x7f040019;
+        public static final int diamond9=0x7f04001a;
+        public static final int felt_green=0x7f04001b;
+        public static final int heart1=0x7f04001c;
+        public static final int heart10=0x7f04001d;
+        public static final int heart11=0x7f04001e;
+        public static final int heart12=0x7f04001f;
+        public static final int heart13=0x7f040020;
+        public static final int heart2=0x7f040021;
+        public static final int heart3=0x7f040022;
+        public static final int heart4=0x7f040023;
+        public static final int heart5=0x7f040024;
+        public static final int heart6=0x7f040025;
+        public static final int heart7=0x7f040026;
+        public static final int heart8=0x7f040027;
+        public static final int heart9=0x7f040028;
+        public static final int spade1=0x7f040029;
+        public static final int spade10=0x7f04002a;
+        public static final int spade11=0x7f04002b;
+        public static final int spade12=0x7f04002c;
+        public static final int spade13=0x7f04002d;
+        public static final int spade2=0x7f04002e;
+        public static final int spade3=0x7f04002f;
+        public static final int spade4=0x7f040030;
+        public static final int spade5=0x7f040031;
+        public static final int spade6=0x7f040032;
+        public static final int spade7=0x7f040033;
+        public static final int spade8=0x7f040034;
+        public static final int spade9=0x7f040035;
+    }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050002;
-        public static final int dummy_content=0x7f050001;
+        public static final int app_name=0x7f060000;
+        public static final int dummy_button=0x7f060002;
+        public static final int dummy_content=0x7f060001;
     }
     public static final class style {
         /** 
@@ -62,15 +118,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060002;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060004;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070002;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070004;
     }
     public static final class styleable {
         /** 
